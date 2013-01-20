a16276d726d93dae9987cf9f34d91d1102027c31
Maxime <maxime.benoit27@gmail.com>
2013-01-20 22:29:41 +0100

    Ajout getAll pour Client et Operateur

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author saturne
  */
 @Entity
+@NamedQuery(name="clientGetAll",query="SELECT a FROM Client a")
 public class Client implements Serializable{
     private static final long serialVersionUID = 1L;
 
--- src/metier/Operateur.java
+++ src/metier/Operateur.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author saturne
  */
 @Entity
+@NamedQuery(name="operateurGetAll",query="SELECT a FROM Operateur a")
 public class Operateur implements Serializable{
     private static final long serialVersionUID = 1L;
 
